4e0eb2bf5b0e21da17a5679c7928be053fd65131
Brad DeFauw <badefauw@gmail.com>
2022-07-05 11:31:09 -0500

    A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function modulo(a, b) {
+    return a % b;
+}
+
+const calculator = {add, subtract, divide, multiply, modulo};
+
+
+module.exports = calculator;
