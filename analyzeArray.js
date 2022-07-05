7a21848b570fb230d67eb2058d9cfc3648989c03
Brad DeFauw <badefauw@gmail.com>
2022-07-05 13:23:07 -0500

    test: An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.

---
+++ analyzeArray.js
@@ -0,0 +1,9 @@
+function analyzeArray(arr){
+    const avg = arr.reduce((a, b) => a + b) / arr.length;
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const length = arr.length;
+    return `average: ${avg}, min: ${min}, max: ${max}, length: ${length};`
+}
+
+module.exports = analyzeArray;
